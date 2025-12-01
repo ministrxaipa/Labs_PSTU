b200b5fa1c2e10bb6f6d9a116ed02ebcccf7c0c5
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2025-12-01 23:04:03 +0500

    Add files via upload

---
+++ sem_1/1_2/1.2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float a,b;
+	cin>>a>>b;
+	if (b==0){
+	    cout<<"на 0 делить нельзя"<<endl;
+	}
+	else{
+	    cout<<a/b<<endl;
+	}
+return 0;
+}
