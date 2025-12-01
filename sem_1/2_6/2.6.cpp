35ffab5c9088d90c2ad39332f389468fa1081106
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2025-12-02 00:16:34 +0500

    Add files via upload

---
+++ sem_1/2_6/2.6.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    if (n>2){
+    for (int i=0;i<n;i++){
+        for (int j=1;j<=i;j++){
+            cout<<" ";
+        }
+        for (int j=0;j<n-i;j++){
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    }
+    else{
+        cout<<"нельзя построить треугольник";
+    }
+    return 0;
+}
