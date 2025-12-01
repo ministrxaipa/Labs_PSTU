6d8b6678a477ea331e03f32ca49fb5804183911f
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2025-12-01 23:21:33 +0500

    Add files via upload

---
+++ sem_1/1_3/1.3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	float n;
+	cin>>n;
+	if (n<5){
+	    cout<<n*3;
+	}
+	else if (n>7){
+	    cout<<n+3;
+	}
+	else{
+	    cout<<n/10;
+	}
+	return 0;
+
+}
