61910bda09f75425302ed5aaf057f43966343e3e
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2025-12-09 02:03:56 +0500

    Add files via upload

---
+++ sem_1/2_13/2.13.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int a,b,c,d,x1,x2;
+    cin>>a>>b>>c;
+    d=pow(b,2)-4*a*c;
+    if (d>0){
+        x1=(-b+sqrt(d))/(2*a);
+        x2=(-b-sqrt(d))/(2*a);
+        cout<<"x1="<<x1<<"; x2="<<x2<<endl;
+    }
+    else if (d==0){
+        x1=(-b)/(2*a);
+        cout<<"x1=x2="<<x1<<endl;
+    }
+    else{
+        cout<<"корней нет"<<endl;
+    }
+    return 0;
+}
