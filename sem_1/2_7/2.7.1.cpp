79cd25eff6f2ada5e94956db754bc0f5e6aefd6f
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2025-12-08 22:32:09 +0500

    Add files via upload

---
+++ sem_1/2_7/2.7.1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+int main() {
+    int n,k;
+    cin>>n;
+    k=sqrt(n);
+    for (int i=0;i<k;i++){
+        for (int j=0;j<k;j++){
+            cout<<" * ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
