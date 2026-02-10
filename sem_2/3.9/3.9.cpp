d24ffc9781720ebbb7e7af9f260a4cae55504162
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2026-02-11 00:01:29 +0500

    Add files via upload

---
+++ sem_2/3.9/3.9.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    const int n=8;
+    int arr[n]={1,2,3,4,5,6,7,8};
+    for (int i=0;i<n;i++){
+        if(arr[i]%2!=0){
+            cout<<"";
+        }
+        else{
+            cout<<arr[i]<<" ";
+        }
+    }
+    cout<<endl;
+    return 0;
+}
