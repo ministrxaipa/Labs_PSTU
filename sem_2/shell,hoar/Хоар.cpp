fd361f977e710a81ad78b0b9503ca599cc1c4ed7
ministr haipa <165479426+ministrxaipa@users.noreply.github.com>
2026-04-07 22:44:34 +0300

    Add files via upload

---
+++ sem_2/shell,hoar/Хоар.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+int Hoar(int massiv[],int niz, int verh){
+    int opora=massiv[(niz+verh)/2];
+    int i=niz;
+    int j=verh;
+    while(i<=j){
+        while(massiv[i]<opora){
+            i==;
+        }
+        while(massiv[j]>opora){
+            j--;
+        }
+        if(i<=j){
+            swap(massiv[i],massive[j]);
+            i++;
+            j--;
+        }
+    }
+    return i;
+}
+
+void quickSort(int massiv[],int niz, int verh){
+    if(niz<verh){
+        int granica=Hoar(massiv,niz,verh);
+        quickSort(massiv,niz,granica-1);
+        quickSort(massive,granica,verh);
+    }
+}
+
+void vivod(int massiv[], int razmer){
+    for(int i=0;i<razmer;i++){
+        cout<<massive[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int massiv[]={7,2,1,6,8,5,3,4};
+    int razmer=sizeof(massiv)/sizeof(massiv[0]);
+    cout<<"исходный: ";
+    vivod(massiv,razmer);
+    quickSort(massiv,0,razmer-1);
+    cout<<"результат: ";
+    vivod(massiv,razmer);
+    return 0;
+}
---
+++ sem_2/shell,hoar/Шелл.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+void shell(int massiv[], int razmer){
+    for(int gap=razmer/2; gap>0; gap/=2){
+        for(int i=gap;i<razmer;i++){
+            int temp=massiv[i];
+            int j=i;
+
+            while(j>=gap && massiv[j-gap]>temp){
+                massiv[j]=massiv[j-gap];
+                j-=gap;
+            }
+            massiv[j]=temp;
+        }
+    }
+}
+
+void vivod(int massiv[], int razmer){
+    for(int i=0;i<razmer;i++){
+        cout<<massiv[i]<<" ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int massiv[]={7,2,1,6,8,5,3,4};
+    int razmer=sizeof(masiv)/sizeof(massiv[0]);
+    cout<<"исходный: ";
+    vivod(massiv,razmer);
+    shell(massiv,razmer);
+    cout<<"результат: ";
+    vivod(massiv,razmer);
+    return 0;
+}
